bf803b76a4cf9c86f2908a0863b8add6da643e19
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-03-10 15:36:37 +0600

    Basic Idea On API Routes

---
+++ src/app/api/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server"
+
+export const GET = async() => {
+return NextResponse.json({message:"hello"})
+}
+
+export const POST = async() => {
+return NextResponse.json({message:"hello"})
+}
+
+export const DELETE = async() => {
+return NextResponse.json({message:"hello"})
+}
+
+export const PUT = async() => {
+return NextResponse.json({message:"hello"})
+}
+export const PATCH = async() => {
+return NextResponse.json({message:"hello"})
+}
