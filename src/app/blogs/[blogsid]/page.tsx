ac0863a95fa05367f83661581b90e1b8faccf449
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-03-10 13:34:41 +0600

    Fetch Dynamic Data Using GenerateStaticParams() For SSG;

--- src/app/blogs/[blogsid]/page.tsx
+++ src/app/blogs/[blogsid]/page.tsx
@@ -1,9 +1,18 @@
+import { Blog } from "@/Types";
 import BlogDetails from "@/components/ui/BlogDetails";
 
 interface blogId {
   params: {
     blogsid: string;
   };
+}
+
+export const generateStaticParams = async() => {
+   const res = await fetch('http://localhost:5000/blogs')
+   const blogs = await res.json()
+   return blogs.slice(0, 3).map((blog:Blog)=>({
+      blogsid: blog.id,
+   }))
 }
 
 const BlogsDetailsPage = async ({ params }: blogId) => {
@@ -15,7 +24,7 @@ const BlogsDetailsPage = async ({ params }: blogId) => {
   // console.log(blog)
 
   return (
-    <div>
+    <div className="my-7">
       <BlogDetails blog={blog} />
     </div>
   );
